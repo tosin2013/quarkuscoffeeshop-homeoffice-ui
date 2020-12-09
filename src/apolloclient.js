4fbacb432db9acc03166dc732ec30f668aa48951
vincent-tsugranes <vtsugranes@gmail.com>
2020-12-09 09:46:09 -0500

    GraphQL Endpoint from env var

--- src/apolloclient.js
+++ src/apolloclient.js
@@ -2,7 +2,13 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 
-const link = new HttpLink({ uri: 'http://0.0.0.0:8080/graphql' })
+console.log("GraphQL Endpoint: " + process.env.GRAPHQL_ENDPOINT);
+const graphqlEndpoint = process.env.GRAPHQL_ENDPOINT;
+if (graphqlEndpoint == undefined){
+  graphqlEndpoint = 'http://0.0.0.0:8080/graphql';
+}
+
+const link = new HttpLink({ uri: graphqlEndpoint })
 const cache = new InMemoryCache()
 const client = new ApolloClient({
   link,
